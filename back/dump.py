a7f2b345ca82b9ef361e90986815b901d8ebfc4d
Yuju Teng <r11725044@ntu.edu.tw>
2023-05-30 01:26:59 +0800

    dump 更改為 function

--- back/dump.py
+++ back/dump.py
@@ -2,38 +2,43 @@ import mysql.connector as m
 import os
 import datetime
 
-# Connecting to db
-db = 'user_dbname'
-connection = m.connect(host='localhost', user='root',password='0000', database=db)
-cur = connection.cursor(buffered=True)
+def dump(user_cur):
 
-# Data for Saving
-data = ""
-# data += "DROP DATABASE IF EXISTS `" + db + "`;\n"
-# data += "CREATE DATABASE `" + db + "`;\n"
-# data += "USE `" + db + "`;\n"
-# data += "\n\n"
+    # Data for Saving
+    data = ""
+    # data += "DROP DATABASE IF EXISTS `" + db + "`;\n"
+    # data += "CREATE DATABASE `" + db + "`;\n"
+    # data += "USE `" + db + "`;\n"
+    # data += "\n\n"
 
-# Getting all table names
-cur.execute('SHOW TABLES;')
-tables = []
-for record in cur.fetchall():
-    tables.append(record[0])
+    # Getting all table names
+    cur.execute('SHOW TABLES;')
+    tables = []
+    for record in cur.fetchall():
+        tables.append(record[0])
 
-for table in tables:
-    # data += "DROP TABLE IF EXISTS `" + str(table) + "`;"
+    for table in tables:
+        # data += "DROP TABLE IF EXISTS `" + str(table) + "`;"
 
-    cur.execute("SHOW CREATE TABLE `" + str(table) + "`;")
-    data += "\n" + str(cur.fetchone()[1]) + ";\n\n"
+        cur.execute("SHOW CREATE TABLE `" + str(table) + "`;")
+        data += "\n" + str(cur.fetchone()[1]) + ";\n\n"
 
 
-# Setting for saving db
-now = datetime.datetime.now()
-filename = str("backup_" + now.strftime("%Y%m%d_%H%M") + ".sql")
-print(filename)
+    # Setting for saving db
+    now = datetime.datetime.now()
+    filename = str("backup_" + now.strftime("%Y%m%d_%H%M") + ".sql")
+    print(filename)
 
-FILE = open(filename,"w")
-FILE.writelines(data)
-FILE.close()
+    file = open(filename,"w")
+    file.writelines(data)
+    file.close()
 
-print("Success")
+    print("Success")
+    
+    
+    
+if __name__ == '__main__':
+    # Connecting to db
+    connection = m.connect(host='localhost', user='root',password='userpw', database='userdb')
+    cur = connection.cursor(buffered=True)
+    dump(cur)
