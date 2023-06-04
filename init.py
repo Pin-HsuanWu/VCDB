f62560dd964732cdd783b61064db45cac9c85439
謝佳穎 <chiayinghsieh212@gmail.com>
2023-06-04 16:04:28 +0800

    add gitignore, add dotenv

--- init.py
+++ init.py
@@ -1,9 +1,15 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 
 if __name__ == '__main__':
+    user = os.getenv('MYSQL_USER')
+    pwd = os.getenv('MYSQL_PASSWORD')
+
     connection1 = mysql.connector.connect(
-        user='root', password='mypassword', host='mysql', port="3306", database='vcdb')  
+        user=user, password=pwd, host='mysql', port="3306", database='vcdb')  
     print("DB connected")
     cursor = connection1.cursor()
     cursor.execute('show tables;')
@@ -11,7 +17,7 @@ if __name__ == '__main__':
     print(students)
 
     connection2 = mysql.connector.connect(
-        user='root', password='mypassword', host='mysql', port="3306", database='userdb')  #壹定要用root為user to 33062
+        user=user, password=pwd, host='mysql', port="3306", database='userdb')  #壹定要用root為user to 33062
     cursor = connection2.cursor()
     cursor.execute('show tables;')
     alls = cursor.fetchall()
