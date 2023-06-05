a68421ac88d49f3b27a21db6bcdfad5983acb8c9
林品歷 <r11725047@ntu.edu.tw>
2023-06-05 09:13:06 +0800

    work in progress

--- GUI.py
+++ GUI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 import user
 
@@ -37,7 +38,7 @@ class MyApp(tk.Tk):
             label="Login", command=lambda: self.show_frame(LoginPage)
         )
 
-        for F in (StartPage, InitPage, RegisterPage, LoginPage):
+        for F in (StartPage, InitPage, RegisterPage, LoginPage, DashboardPage):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -47,7 +48,6 @@ class MyApp(tk.Tk):
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
-
 
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -72,21 +72,25 @@ class InitPage(tk.Frame):
         user_label = tk.Label(self, text='User:')
         user_label.grid(row=1, column=0, sticky='e')
         user_entry = tk.Entry(self, textvariable=user_var)
+        user_entry.insert(0, 'root')
         user_entry.grid(row=1, column=1)
 
         pwd_label = tk.Label(self, text='Password:')
         pwd_label.grid(row=2, column=0, sticky='e')
         pwd_entry = tk.Entry(self, textvariable=pwd_var)
+        pwd_entry.insert(0, 'secure1234')
         pwd_entry.grid(row=2, column=1)
 
         host_label = tk.Label(self, text='Host:')
         host_label.grid(row=3, column=0, sticky='e')
         host_entry = tk.Entry(self, textvariable=host_var)
+        host_entry.insert(0, '127.0.0.1')
         host_entry.grid(row=3, column=1)
 
         port_label = tk.Label(self, text='Port:')
         port_label.grid(row=4, column=0, sticky='e')
         port_entry = tk.Entry(self, textvariable=port_var)
+        port_entry.insert(0, '3306')
         port_entry.grid(row=4, column=1)
 
         parse_button = tk.Button(
@@ -164,11 +168,13 @@ class LoginPage(tk.Frame):
         name_label = tk.Label(self, text='User Name:')
         name_label.grid(row=1, column=0, sticky='e')
         name_entry = tk.Entry(self, textvariable=name_var)
+        name_entry.insert(0, 'Leo')
         name_entry.grid(row=1, column=1)
 
         email_label = tk.Label(self, text='User Email:')
         email_label.grid(row=2, column=0, sticky='e')
         email_entry = tk.Entry(self, textvariable=email_var)
+        email_entry.insert(0, 'Leo@gmail.com')
         email_entry.grid(row=2, column=1)
 
         parse_button = tk.Button(
@@ -187,9 +193,53 @@ class LoginPage(tk.Frame):
             current_version, current_branch = user.login(db_cursor, database_name, name, email)
             messagebox.showinfo('Login', "Login successfully.")
             print(current_version, current_branch)
+            self.controller.show_frame(DashboardPage)
         except Exception as e:
             print(e)
             messagebox.showinfo('Login', e)
+
+# After login show the page below
+
+class DashboardPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        label = tk.Label(self, text="Dashboard")
+        label.grid(row=0, column=0, columnspan=2, pady=10, padx=10)
+
+        commit_button = tk.Button(
+            self,
+            text='Commit',
+            command=self.commit_changes
+        )
+        commit_button.grid(row=1, column=0, pady=10)
+
+        log_button = tk.Button(
+            self,
+            text='Log',
+            command=self.view_log
+        )
+        log_button.grid(row=1, column=1, pady=10)
+
+        merge_button = tk.Button(
+            self,
+            text='Merge',
+            command=self.merge_branches
+        )
+        merge_button.grid(row=1, column=2, columnspan=2, pady=10)
+
+    def commit_changes(self):
+        # Add your commit logic here
+        messagebox.showinfo('Commit', 'Changes committed successfully.')
+
+    def view_log(self):
+        # Add your log viewing logic here
+        messagebox.showinfo('Log', 'Log viewed successfully.')
+
+    def merge_branches(self):
+        # Add your merge logic here
+        messagebox.showinfo('Merge', 'Branches merged successfully.')
 
 
 app = MyApp()
