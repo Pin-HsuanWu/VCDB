2e2b17252b67706321d720912312db202b6b915e
Yuju Teng <r11725044@ntu.edu.tw>
2023-06-06 13:13:10 +0800

    新增使用 globals 的版本 (未測試)

--- app/hop.py
+++ app/hop.py
@@ -1,4 +1,110 @@
 import mysql.connector as m
+import globals
+
+def hop(destination):
+    # get 目前 user 的相關資訊
+    query = "SELECT * FROM user WHERE uid = '%s';" % globals.current_uid
+    globals.vc_cursor.execute(query)
+    userInfo = globals.vc_cursor.fetchone()
+
+    if (userInfo[3] == None):  # 沒有 user 的 current_version 的紀錄
+        print("Cannot find where you are!")
+        return
+
+    origin = str(userInfo[3])  # 起點
+    
+    query = "SELECT * FROM commit WHERE version = '%s';" % origin
+    globals.vc_cursor.execute(query)
+    originInfo = globals.vc_cursor.fetchone()
+    originTime = originInfo[5]
+    # print(originTime)
+    
+    # get hop direction
+    query = "SELECT * FROM commit WHERE version = '%s';" % destination
+    globals.vc_cursor.execute(query)
+    destinationInfo = globals.vc_cursor.fetchone()
+
+    if (destinationInfo == None):  # 沒有 destination 的紀錄
+        print("Cannot find the destination!")
+        return
+
+    destinationTime = destinationInfo[5]
+    # print(destinationTime)
+    
+    alterData = "SET foreign_key_checks = 0;\n"
+    found = False
+    hopCount = 0
+    
+    # 確認 hop 方向
+    if (destinationTime < originTime):  # past
+        # 中繼點
+        relay = origin
+        relayBranch = str(userInfo[1])
+
+        while (relay != None):
+            # print("relay: ", relay)
+            # 找出下個要去的點
+            query = "SELECT * FROM commit WHERE version = '%s';" % relay
+            globals.vc_cursor.execute(query)
+            relayInfo = globals.vc_cursor.fetchone()
+
+            if (relayInfo == None): break
+
+            alterData += str(relayInfo[4]) + "\n"  # downgrade
+            relay = str(relayInfo[2])
+            relayBranch = str(relayInfo[1])
+
+            hopCount += 1
+            
+            if (destination == relay):
+                found = True
+                break
+            
+    elif (destinationTime > originTime):  # future
+        # 中繼點
+        relay = origin
+        relayBranch = str(userInfo[1])
+    
+        while (relay != None):
+            # print("relay: ", relay)
+            # 找出下個要去的點
+            query = "SELECT * FROM commit WHERE last_version = '%s';" % relay
+            globals.vc_cursor.execute(query)
+            relayInfo = globals.vc_cursor.fetchone()
+            
+            if (relayInfo == None):  break
+            
+            alterData += str(relayInfo[3]) + "\n"  # upgrade
+            relay = str(relayInfo[0])
+            relayBranch = str(relayInfo[1])
+
+            hopCount += 1
+            
+            if (destination == relay):
+                found = True
+                break
+    else:
+        print('You do not need to hop! You are here already!')
+        return
+    
+    
+    # 確認是否可以更新
+    if (found == True):
+        # 更新 vc 的 user table
+        update = "UPDATE user SET current_version = (%s), current_bid = (%s) WHERE uid = (%s);"
+        val = (destination, relayBranch, globals.current_uid)
+        globals.vc_cursor.execute(update, val)
+        vc_connection.commit()
+
+        # 執行更新
+        # print(alterData)
+        globals.user_cursor.execute(alterData)
+
+        print("Successfully hop and you hop", hopCount, "commit(s)!")
+        
+    else:
+        print('Cannot find the destination!')
+
 
 
 def hop(vc_connection, user_cur, vc_cur, userID, destination):
@@ -105,6 +211,7 @@ def hop(vc_connection, user_cur, vc_cur, userID, destination):
         
     else:
         print('Cannot find the destination!')
+
 
 
 if __name__ == '__main__':
