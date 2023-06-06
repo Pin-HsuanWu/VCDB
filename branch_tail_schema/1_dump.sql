45c94598663b653ae3bf1f014abbf914a85654a3
Yuju Teng <r11725044@ntu.edu.tw>
2023-06-06 10:36:38 +0800

    commit 修改: 1st commit 時對應的處理 / 新增: 使用 globals 的版本

--- branch_tail_schema/1_dump.sql
+++ branch_tail_schema/1_dump.sql
@@ -1,29 +1,26 @@
 
-CREATE TABLE `course` (
-  `Id` varchar(20) NOT NULL,
-  `Name` varchar(20) NOT NULL,
-  `TeacherID` varchar(20) NOT NULL,
-  `Credit` int(11) NOT NULL,
-  KEY `TeacherID` (`TeacherID`),
-  CONSTRAINT `course_ibfk_1` FOREIGN KEY (`TeacherID`) REFERENCES `teacher` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+CREATE TABLE `development_team` (
+  `Name` varchar(40) NOT NULL,
+  PRIMARY KEY (`Name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+
+CREATE TABLE `game` (
+  `Title` varchar(40) NOT NULL,
+  `Year` int(11) DEFAULT NULL CHECK (`Year` > 1800 and `Year` < 2100),
+  `Price` int(11) DEFAULT NULL,
+  `Development_team` varchar(40) NOT NULL,
+  PRIMARY KEY (`Title`),
+  KEY `Development_team` (`Development_team`),
+  CONSTRAINT `game_ibfk_1` FOREIGN KEY (`Development_team`) REFERENCES `development_team` (`Name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 
 CREATE TABLE `student` (
   `Id` varchar(20) NOT NULL,
   `Name` varchar(20) NOT NULL,
-  `Grade` int(11) NOT NULL,
-  `Department` varchar(20) NOT NULL,
-  PRIMARY KEY (`Id`),
-  CONSTRAINT `CONSTRAINT_1` CHECK (`Grade` > 0)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
-
-CREATE TABLE `teacher` (
-  `Id` varchar(20) NOT NULL,
-  `Name` varchar(10) NOT NULL,
-  `Department` varchar(20) NOT NULL,
-  `age` int(11) DEFAULT NULL,
+  `Grade` int(11) DEFAULT NULL,
+  `Department` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
