d45a04607bc3d49659358b0d4acbee8cb5a0a293
謝佳穎 <chiayinghsieh212@gmail.com>
2023-06-08 10:09:57 +0800

    remove revert.py / dump.py remains the same

--- app/dump.py
+++ app/dump.py
@@ -1,12 +1,15 @@
-import mysql.connector
+import mysql.connector as m
 import os
 import datetime
-
 
 def dump(cur):
 
     # Data for Saving
     data = ""
+    # data += "DROP DATABASE IF EXISTS `" + db + "`;\n"
+    # data += "CREATE DATABASE `" + db + "`;\n"
+    # data += "USE `" + db + "`;\n"
+    # data += "\n\n"
 
     # Getting all table names
     cur.execute('SHOW TABLES;')
@@ -15,20 +18,34 @@ def dump(cur):
         tables.append(record[0])
 
     for table in tables:
+        # data += "DROP TABLE IF EXISTS `" + str(table) + "`;"
+
         cur.execute("SHOW CREATE TABLE `" + str(table) + "`;")
         data += "\n" + str(cur.fetchone()[1]) + ";\n\n"
 
+
     # Setting for saving db
     now = datetime.datetime.now()
-    filename = str("tmpfile" + ".sql")
+    path = "./branch_tail_schema"
+    filename = str("backup_" + now.strftime("%Y%m%d_%H%M") + ".sql")
 
-    file = open(filename, "w")
+    file = open(os.path.join(path, filename),"w")
     file.writelines(data)
     file.close()
 
-    print("Successfully dump " + filename)
-
+    #print("Successfully dump " + filename)
+    
     return filename
-
-
-#dump(user_cursor)
+    
+    
+    
+if __name__ == '__main__':
+    # Connect to db
+    connection = m.connect(host='localhost', user='root',password='secure1234', database='db_class')
+    cur = connection.cursor(buffered=True)
+    
+    dump(cur)
+    
+    # Close connection
+    cur.close()
+    connection.close()
