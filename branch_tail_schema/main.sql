a1ad4371a40f45918e8ce80f38b972b8ecd2ea71
謝佳穎 <chiayinghsieh212@gmail.com>
2023-06-09 09:28:23 +0800

    pwd displays as ****

--- branch_tail_schema/main.sql
+++ branch_tail_schema/main.sql
@@ -9,11 +9,11 @@ CREATE TABLE `teacher` (
 
 
 CREATE TABLE `test` (
-  `Department` varchar(20) NOT NULL,
-  `Id` varchar(20) NOT NULL,
+  `test` varchar(45) DEFAULT NULL,
   `Grade` int NOT NULL,
   `Name` varchar(20) NOT NULL,
-  `test` varchar(45) DEFAULT NULL,
+  `Department` varchar(20) NOT NULL,
+  `Id` varchar(20) NOT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
